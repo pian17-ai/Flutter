6dadad3c8d495c167564b6aabc39e72639431989
pian17-ai <alviancahyopambudi@gmail.com>
2025-10-21 12:02:29 +0700

    update

--- Bu-Lia/latihan_8/lib/AddProduct.dart
+++ Bu-Lia/latihan_8/lib/AddProduct.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:latihan_8/ProductPage.dart';
+import 'package:http/http.dart' as http;
 
 class AddProduct extends StatefulWidget {
   const AddProduct({super.key});
@@ -8,13 +10,27 @@ class AddProduct extends StatefulWidget {
 }
 
 class _AddProductState extends State<AddProduct> {
+  final formKey = GlobalKey<FormState>();
+  TextEditingController nama_product = TextEditingController();
+  TextEditingController price_product = TextEditingController();
+  Future _simpan() async {
+    final respon = await http.post
+    (Uri.parse('http://localhost/PHP/Product_API/create.php'), body:{
+      'name_product':nama_product.text,
+      'price_product':price_product.text,
+    });
+    if(respon.statusCode == 200) {
+      return true;
+    }
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Tambah product")),
 
       body: Form(
-        key: formkey,
+        key: formKey,
         child: Container(
           padding: EdgeInsets.all(20),
           child: Column(
@@ -25,9 +41,60 @@ class _AddProductState extends State<AddProduct> {
                   hintText: 'Product name',
                   border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(12),
-                  ),
-                ),
+                  )),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Name product tidak boleh kosong';
+                  }
+                },
               ),
+
+              SizedBox(height: 10),
+
+              TextFormField(
+                controller: price_product,
+                decoration: InputDecoration(
+                  hintText: 'Product price',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  )),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Product price tidak boleh kosong';
+                  }
+                },
+              ),
+
+
+              SizedBox(height: 10),
+              ElevatedButton(
+                onPressed: () {
+                  if (formKey.currentState!.validate()) {
+                    _simpan().then((value) {
+                      if (value) {
+                        final snackBar=SnackBar(
+                          content: const Text('Data berhasil disimpan'),
+                          );
+                          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                      } else {
+                        final snackBar = SnackBar(
+                          content: const Text('Data gagal disimpan'),
+                        );
+                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                      }
+                    }
+                    );
+
+                    Navigator.pushAndRemoveUntil(
+                      context, 
+                      MaterialPageRoute(builder: (context)=>Productpage(),
+                      ), 
+                      (Route <dynamic> route)=>false,
+                    );
+                  }
+                },
+                child: const Text('Simpan'),
+                ),
             ],
           ),
         ),
