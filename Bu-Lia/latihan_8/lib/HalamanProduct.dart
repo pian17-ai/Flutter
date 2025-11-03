3ee2c9f8f5fff8301c67fd364d7826f1856bbe24
pian17-ai <alviancahyopambudi@gmail.com>
2025-11-03 14:01:57 +0700

    add delete and fixed edited bug

--- Bu-Lia/latihan_8/lib/HalamanProduct.dart
+++ Bu-Lia/latihan_8/lib/HalamanProduct.dart
@@ -14,53 +14,54 @@ class HalamanProduk extends StatefulWidget {
 }
 
 class _HalamanProdukState extends State<HalamanProduk> {
-List _listdata = [];
-bool _loading = true;
+  List _listdata = [];
+  bool _loading = true;
 
-Future _getData() async{
-  try {
-    final respon = 
-    await http.get(Uri.parse('http://localhost/PHP/Product_API/read.php'));
-    if (respon.statusCode == 200){
-      final data = jsonDecode(respon.body);
-      setState(() {
-        _listdata = data;
-        _loading = false;
-      });
-     } 
-     } catch (e) {
+  Future _getData() async {
+    try {
+      final respon = await http.get(
+        Uri.parse('http://localhost/PHP/Product_API/read.php'),
+      );
+      if (respon.statusCode == 200) {
+        final data = jsonDecode(respon.body);
+        setState(() {
+          _listdata = data;
+          _loading = false;
+        });
+      }
+    } catch (e) {
       print(e);
-     }
-}
+    }
+  }
 
-// Future<void> _deleteProduk(String idProduk) async {
-//   try{
-//     final respon=
-//     await http.post(Uri.parse('http://localhost/fayola/delete.php'), body:{
-//       'id_produk':idProduk,
-//     });
-//     if (respon.statusCode==200){
-//       setState((){
-//         _listdata.removeWhere((item) => item['id_produk'] == idProduk);
-//       });
-//       ScaffoldMessenger.of(context).showSnackBar(
-//         SnackBar(content:Text('Produk berhasil berhapus')),
-//         );
-//     } else{
-//       throw Exception('Gagal menghapus produk');
-//     }
-//   } catch (e){
-//     print(e);
-//     ScaffoldMessenger.of(context).showSnackBar(
-//       SnackBar(content: Text('Terjadi Kesalahan')),
-//       );
-//   }
-// }
+  Future<void> _deleteProduk(String idProduk) async {
+    try {
+      final respon = await http.post(
+        Uri.parse('http://localhost/PHP/Product_API/delete.php'),
+        body: {'id_product': idProduk},
+      );
+      if (respon.statusCode == 200) {
+        setState(() {
+          _listdata.removeWhere((item) => item['id_product'] == idProduk);
+        });
+        ScaffoldMessenger.of(
+          context,
+        ).showSnackBar(SnackBar(content: Text('Produk berhasil berhapus')));
+      } else {
+        throw Exception('Gagal menghapus produk');
+      }
+    } catch (e) {
+      print(e);
+      ScaffoldMessenger.of(
+        context,
+      ).showSnackBar(SnackBar(content: Text('Terjadi Kesalahan')));
+    }
+  }
 
-void initState(){
-  _getData();
-  super.initState();
-}
+  void initState() {
+    _getData();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -69,63 +70,99 @@ void initState(){
         title: Text('Halaman Produk'),
         backgroundColor: Colors.pink,
       ),
-      body:_loading 
-      ?Center(
-        child: CircularProgressIndicator(),
-      )
-      : ListView.builder(
-        itemCount: _listdata.length,
-        itemBuilder: ((context, index) {
-          return Card(
-            child: InkWell(
-              onTap: (){
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => DetailProduk(
-                      Listdata:{
-                        'id_product': _listdata[index]
-                        ['id_product'],
-                        'name_product':_listdata[index]
-                        ['name_product'], 
-                        'price_product':_listdata[index]
-                        ['price_product'],
-                      },
-                    )));
-              },
-              child:ListTile(
-                title:Text(_listdata[index]['name_product'].toString()),
-                subtitle: Text (_listdata[index]['price_product']),
-                trailing :IconButton(
-                  onPressed: (){
-                    Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) => UbahProduk(
-                      Listdata:{
-                        'id_product': _listdata[index]['id_product'],
-                        'name_product':_listdata[index]['name_product'], 
-                        'price_product':_listdata[index]['price_product'],
-                      },
-                    )));
-              },
-              icon: Icon(Icons.edit),
-                ),
-              ),
+      body: _loading
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+              itemCount: _listdata.length,
+              itemBuilder: ((context, index) {
+                return Card(
+                  child: InkWell(
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => DetailProduk(
+                            Listdata: {
+                              'id_product': _listdata[index]['id_product'],
+                              'name_product': _listdata[index]['name_product'],
+                              'price_product':
+                                  _listdata[index]['price_product'],
+                            },
+                          ),
+                        ),
+                      );
+                    },
+                    child: ListTile(
+                      title: Text(_listdata[index]['name_product'].toString()),
+                      subtitle: Text(_listdata[index]['price_product']),
+                      trailing: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          IconButton(
+                            icon: Icon(Icons.edit, color: Colors.blue),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => UbahProduk(
+                                    Listdata: {
+                                      'id_product':
+                                          _listdata[index]['id_product'],
+                                      'name_product':
+                                          _listdata[index]['name_product'],
+                                      'price_product':
+                                          _listdata[index]['price_product'],
+                                    },
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                          IconButton(
+                            icon: Icon(Icons.delete, color: Colors.redAccent),
+                            onPressed: () {
+                              showDialog(
+                                context: context,
+                                builder: (context) => AlertDialog(
+                                  title: Text("Hapus Data"),
+                                  content: Text(
+                                    'Apakah mau menghapus product ini.',
+                                  ),
+                                  actions: [
+                                    TextButton(
+                                      onPressed: () => Navigator.pop(context),
+                                      child: Text('Batal'),
+                                    ),
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.pop(context);
+                                        _deleteProduk(
+                                          _listdata[index]['id_product'],
+                                        );
+                                      },
+                                      child: Text("Hapus"),
+                                    ),
+                                  ],
+                                ),
+                              );
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              }),
             ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('+', style: TextStyle(fontSize: 20)),
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => TambahProduk()),
           );
-        }),
-    ),
-     floatingActionButton: FloatingActionButton(
-      child:Text(
-        '+',
-        style:TextStyle(fontSize:20),
+        },
       ),
-      onPressed: (){
-        Navigator.push(
-          context,MaterialPageRoute(builder: (context) => TambahProduk()));
-      },
-     ),
     );
   }
 }
